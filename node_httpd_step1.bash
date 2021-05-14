5ccea15d980be2f214686cbe1a016d1ce452ba22
seriesninefournine <78902612+seriesninefournine@users.noreply.github.com>
2021-05-15 01:01:43 +0500

    Update node_httpd_step1.bash

--- node_httpd_step1.bash
+++ node_httpd_step1.bash
@@ -18,9 +18,23 @@ echo "
 192.168.10.21 node03.local 
 192.168.10.23 node04.local" >> /etc/hosts
 
-#Подготавливаем раздел для хранения данных кластера
-mkdir /opt/gluster
-
 #Запускаем кластер
 sudo systemctl enable glusterd
 sudo systemctl start glusterd
+
+#Получаем Ip адрес ноды
+NodeIP=$(ip addr  | grep 'inet'| egrep -v '127.0.0.1|inet6' | cut -b 10-22)
+
+#Запускаем добавление узлов кластера только на одном из участников кластера 
+
+if [ $NodeIP == "192.168.10.20" ]; then
+  #Добавляем узлы кластера в пул
+  gluster peer probe  node02.local
+  gluster peer probe  node03.local
+  gluster peer probe  node04.local
+
+  #Запускаем кластер
+  #gluster volume create httpd_data replica 3 node0{2,3,4}.local:/opt/gluster-volume force
+fi
+
+gluster volume create httpd_data replica 3 $HOSTNAME:/opt/gluster-volume force
