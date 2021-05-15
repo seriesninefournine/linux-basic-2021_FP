ad010282d88518589cd60c1a1967ad1d0b8ff98b
seriesninefournine <78902612+seriesninefournine@users.noreply.github.com>
2021-05-15 11:50:30 +0500

    Add files via upload

--- node_httpd_step2.1.bash
+++ node_httpd_step2.1.bash
@@ -4,5 +4,5 @@ yum -y install wget
 wget https://github.com/glpi-project/glpi/releases/download/9.5.5/glpi-9.5.5.tgz
 tar -xzf glpi-9.5.5.tgz
 cd ./glpi
-mv -r ./* /var/www/html/ -f
+mv ./* /var/www/html/
 chown -R apache:apache /var/www
--- node_httpd_step2.bash
+++ node_httpd_step2.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 gluster volume start httpd_data
+mkdir /var/www
+echo 'localhost:/httpd_data /var/www glusterfs defaults,_netdev,backupvolfile-server=localhost 0 0' >> /etc/fstab
+mount -a
 
 #Устанавливаем httpd + php с необходимыми модулями
 rpm -Uvh http://rpms.remirepo.net/enterprise/remi-release-7.rpm
@@ -9,4 +12,4 @@ yum-config-manager --disable remi-safe
 yum-config-manager --enable remi
 yum-config-manager --enable remi-php74
 
-yum -y install httpd php74 wget
+yum -y install httpd php74
