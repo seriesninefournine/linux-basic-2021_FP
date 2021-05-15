f9bb84b555c3d98a77497a518fc4af1ca0bcfc8e
seriesninefournine <78902612+seriesninefournine@users.noreply.github.com>
2021-05-15 10:09:50 +0500

    Update node_httpd_step1.1.bash

--- node_httpd_step1.1.bash
+++ node_httpd_step1.1.bash
@@ -4,4 +4,5 @@
 gluster peer probe  node02.local
 gluster peer probe  node03.local
 gluster peer probe  node04.local
+sleep 3
 gluster volume create httpd_data replica 3 node0{2,3,4}.local:/opt/gluster-volume force
