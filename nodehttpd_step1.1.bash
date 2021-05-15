88a06ebfa88680d2fb27403f33925aaf38972806
seriesninefournine <78902612+seriesninefournine@users.noreply.github.com>
2021-05-15 10:03:15 +0500

    Delete nodehttpd_step1.1.bash

--- nodehttpd_step1.1.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Запускаем добавление узлов кластера только на одном из участников кластера 
-gluster peer probe  node02.local
-gluster peer probe  node03.local
-gluster peer probe  node04.local
-gluster volume create httpd_data replica 3 node0{2,3,4}.local:/opt/gluster-volume force
