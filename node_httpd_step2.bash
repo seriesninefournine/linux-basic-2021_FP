9060ae58c18dfad39d75a2cb1503fd647dbf8aef
seriesninefournine <78902612+seriesninefournine@users.noreply.github.com>
2021-05-15 10:36:01 +0500

    Add files via upload

--- node_httpd_step2.bash
+++ node_httpd_step2.bash
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-mkdir /var/www
-echo "localhost:/httpd_data /var/www glusterfs defaults,_netdev,backupvolfile-server=localhost 0 0" >> /etc/fstab
 gluster volume start httpd_data
-mount -a
 
 #Устанавливаем httpd + php с необходимыми модулями
 rpm -Uvh http://rpms.remirepo.net/enterprise/remi-release-7.rpm
