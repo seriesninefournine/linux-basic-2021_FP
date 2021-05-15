afd9419786f49dd61f627316a54d5a167327bf83
seriesninefournine <78902612+seriesninefournine@users.noreply.github.com>
2021-05-15 10:34:58 +0500

    Add files via upload

--- node_httpd_step1.1.bash
+++ node_httpd_step1.1.bash
@@ -5,4 +5,4 @@ gluster peer probe  node02.local
 gluster peer probe  node03.local
 gluster peer probe  node04.local
 sleep 3
-gluster volume create httpd_data replica 3 node0{2,3,4}.local:/opt/gluster-volume force
+gluster volume create httpd_data replica 3 node0{2,3,4}.local:/var/www force
