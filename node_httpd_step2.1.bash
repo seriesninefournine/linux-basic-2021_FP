382bf0bfd7cb92f03d38c031e818c21c9df96b3e
seriesninefournine <78902612+seriesninefournine@users.noreply.github.com>
2021-05-15 10:21:46 +0500

    Update node_httpd_step2.1.bash

--- node_httpd_step2.1.bash
+++ node_httpd_step2.1.bash
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 yum -y install wget
-mkdir ./glpi
-cd ./glpi
-wget --no-check-certificate https://github.com/glpi-project/glpi/releases/download/9.5.5/glpi-9.5.5.tgz
+wget https://github.com/glpi-project/glpi/releases/download/9.5.5/glpi-9.5.5.tgz
 tar -xzf glpi-9.5.5.tgz
 cd ./glpi
-
