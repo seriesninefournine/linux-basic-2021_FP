6e336556288890bf28d95cad17b9e23eac0e5551
seriesninefournine <78902612+seriesninefournine@users.noreply.github.com>
2021-05-23 23:10:07 +0500

    Update node_httpd_step1.1.bash

--- node_httpd_step1.1.bash
+++ node_httpd_step1.1.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Запускаем добавление узлов кластера только на одном из участников кластера 
-gluster peer probe  node02.local
 gluster peer probe  node03.local
 gluster peer probe  node04.local
+gluster peer probe  node05.local
 sleep 3
-gluster volume create httpd_data replica 3 node0{2,3,4}.local:/opt/gluster-volume force
+gluster volume create httpd_data replica 3 arbiter 1 node0{3,4,5}.local:/opt/gluster-volume force
