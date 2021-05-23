33d15fd8018af14b4f9238058ca58904ac16894d
seriesninefournine <78902612+seriesninefournine@users.noreply.github.com>
2021-05-24 00:07:36 +0500

    Update node_httpd_step2.bash

--- node_httpd_step2.bash
+++ node_httpd_step2.bash
@@ -2,8 +2,26 @@
 
 gluster volume start httpd_data
 mkdir /mnt/gluster
-echo 'localhost:/httpd_data /mnt/gluster glusterfs defaults,_netdev,backupvolfile-server=localhost 0 0' >> /etc/fstab
-mount -a
+
+echo "[Unit]
+Description=gluster mount
+[Mount]
+What=localhost:/httpd_data
+Where=/mnt/gluster
+Type=glusterfs
+Options=defaults,_netdev,backupvolfile-server=localhost" > /etc/systemd/system/mnt-gluster.mount
+
+echo "[Unit]
+Description=gluster mount
+Requires=network-online.target
+[Automount]
+Where=/mnt/gluster
+TimeoutIdleSec=301
+[Install]
+WantedBy=remote-fs.target" > /etc/systemd/system/mnt-gluster.automount
+
+chmod +x /etc/systemd/system/mnt-gluster.*
+systemctl start mnt-gluster.automount
 
 #Устанавливаем httpd + php с необходимыми модулями
 rpm -Uvh http://rpms.remirepo.net/enterprise/remi-release-7.rpm
