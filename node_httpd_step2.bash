bc82bda9229a5d42c06e87d39e31bc0324a74508
seriesninefournine <78902612+seriesninefournine@users.noreply.github.com>
2021-05-24 15:41:30 +0500

    Update node_httpd_step2.bash

--- node_httpd_step2.bash
+++ node_httpd_step2.bash
@@ -2,8 +2,6 @@
 
 gluster volume start httpd_data
 mkdir /var/www
-echo 'localhost:/httpd_data /var/www glusterfs defaults,_netdev,backupvolfile-server=localhost 0 0' >> /etc/fstab
-mount -a
 
 echo "[Unit]
 Description=gluster mount
@@ -25,12 +23,3 @@ WantedBy=remote-fs.target" > /etc/systemd/system/mnt-gluster.automount
 chmod +x /etc/systemd/system/mnt-gluster.*
 systemctl enable mnt-gluster.automount
 systemctl start mnt-gluster.automount
-
-#Устанавливаем httpd + php с необходимыми модулями
-rpm -Uvh http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-
-yum-config-manager --disable remi-safe
-yum-config-manager --enable remi
-yum-config-manager --enable remi-php74
-
-yum -y install httpd php74
