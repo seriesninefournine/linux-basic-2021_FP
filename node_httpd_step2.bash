5ed5bb441c9f3ecedea4c3a6a7b93af79e30ed75
seriesninefournine <78902612+seriesninefournine@users.noreply.github.com>
2021-05-24 15:50:32 +0500

    Update node_httpd_step2.bash

--- node_httpd_step2.bash
+++ node_httpd_step2.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 gluster volume start httpd_data
-mkdir /var/www
+mkdir /mnt/gluster
 
 echo "[Unit]
 Description=gluster mount
 [Mount]
 What=localhost:/httpd_data
-Where=/var/www
+Where=/mnt/gluster
 Type=glusterfs
 Options=defaults,_netdev,backupvolfile-server=localhost" > /etc/systemd/system/mnt-gluster.mount
 
@@ -15,11 +15,12 @@ echo "[Unit]
 Description=gluster mount
 Requires=network-online.target
 [Automount]
-Where=/var/www
+Where=/mnt/gluster
 TimeoutIdleSec=301
 [Install]
 WantedBy=remote-fs.target" > /etc/systemd/system/mnt-gluster.automount
 
+systemctl daemon-reload
 chmod +x /etc/systemd/system/mnt-gluster.*
 systemctl enable mnt-gluster.automount
 systemctl start mnt-gluster.automount
