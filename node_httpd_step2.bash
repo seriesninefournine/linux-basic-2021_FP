a316c9cb53c7ec5e2cbc30cb0293890999015f93
seriesninefournine <78902612+seriesninefournine@users.noreply.github.com>
2021-05-25 02:13:50 +0500

    Update node_httpd_step2.bash

--- node_httpd_step2.bash
+++ node_httpd_step2.bash
@@ -1,26 +1,36 @@
 #!/bin/bash
 
 gluster volume start httpd_data
-mkdir /mnt/gluster
+mkdir /var/www
 
 echo "[Unit]
 Description=gluster mount
 [Mount]
 What=localhost:/httpd_data
-Where=/mnt/gluster
+Where=/var/www
 Type=glusterfs
-Options=defaults,_netdev,backupvolfile-server=localhost" > /etc/systemd/system/mnt-gluster.mount
+Options=defaults,_netdev,backupvolfile-server=localhost" > /etc/systemd/system/var-www.mount
 
 echo "[Unit]
 Description=gluster mount
 Requires=network-online.target
 [Automount]
-Where=/mnt/gluster
+Where=/var/www
 TimeoutIdleSec=301
 [Install]
-WantedBy=remote-fs.target" > /etc/systemd/system/mnt-gluster.automount
+WantedBy=remote-fs.target" > /etc/systemd/system/var-www.automount
 
+
+chmod +x /etc/systemd/system/var-www.*
 systemctl daemon-reload
-chmod +x /etc/systemd/system/mnt-gluster.*
-systemctl enable mnt-gluster.automount
-systemctl start mnt-gluster.automount
+systemctl enable var-www.automount
+systemctl start var-www.automountww
+
+#Устанавливаем httpd + php с необходимыми модулями
+rpm -Uvh http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+
+yum-config-manager --disable remi-safe
+yum-config-manager --enable remi
+yum-config-manager --enable remi-php74
+
+yum -y install httpd php74
