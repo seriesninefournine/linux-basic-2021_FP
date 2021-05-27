81587b92c4b940a5479dc04d8e6a8c0138ab34ae
seriesninefournine <78902612+seriesninefournine@users.noreply.github.com>
2021-05-27 22:45:22 +0500

    Update node_sql_step1.bash

--- node_sql_step1.bash
+++ node_sql_step1.bash
@@ -18,11 +18,81 @@ systemctl start ntpd
 ntpdate -s ru.pool.ntp.org
 sleep 15
 
-#добавляем адреса узлов кластера
+#добавляем адреса узлов 
 echo "
 192.168.10.121 node06.local 
 192.168.10.122 node07.local" >> /etc/hosts
 
-#Настраиваем pacemaker
+#Запускаем pacemaker
 echo 'pacpass' | passwd --stdin hacluster
-systemctl start pcsd 
+systemctl start pacemaker pcsd corosync 
+
+#Настраиваем MySQL
+echo 'bind-address            = 0.0.0.0
+gtid_mode=ON
+enforce_gtid_consistency=ON' >> /etc/my.cnf
+
+systemctl start mysqld
+MYSQL_PASS='Qq123456!'
+#Автомтизируем mysql_secure_installation
+
+MYSQL_TMP_PASS=$(grep 'temporary password' /var/log/mysqld.log | grep -oE '[^ ]+$')
+
+/usr/bin/expect<<EXP_SCRIPT
+set timeout 10
+spawn mysql_secure_installation
+match_max 100000
+expect -exact "Enter password for user root: "
+send -- "$MYSQL_TMP_PASS\r"
+expect -exact "New password: "
+send -- "$MYSQL_PASS\r"
+expect -exact "Re-enter new password: "
+send -- "$MYSQL_PASS\r"
+expect -exact "Change the password for root ? ((Press y|Y for Yes, any other key for No) : "
+send -- "n\r"
+expect -exact "Remove anonymous users? (Press y|Y for Yes, any other key for No) : "
+send -- "y\r"
+expect -exact "Disallow root login remotely? (Press y|Y for Yes, any other key for No) : "
+send -- "y\r"
+expect -exact "Remove test database and access to it? (Press y|Y for Yes, any other key for No) : "
+send -- "y\r"
+expect -exact "Reload privilege tables now? (Press y|Y for Yes, any other key for No) : "
+send -- "y\r"
+expect eof
+EXP_SCRIPT
+
+
+
+
+if [ $(hostname) == node06.local ]; then
+  echo 'server_id = 1' >> /etc/my.cnf
+  systemctl restart mysqld
+  mysql -u 'root' -p$MYSQL_PASS -e "create user 'replicator07'@'%' IDENTIFIED WITH caching_sha2_password BY '$MYSQL_PASS'; GRANT SELECT, SHOW VIEW, PROCESS, REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'replicator07'@'%'; flush privileges;"
+  mysql -u 'root' -p$MYSQL_PASS -e "STOP SLAVE;"
+  mysql -u 'root' -p$MYSQL_PASS -e "CHANGE MASTER TO MASTER_HOST='node07.local', MASTER_USER='replicator06', MASTER_PASSWORD='$MYSQL_PASS', GET_MASTER_PUBLIC_KEY = 1, MASTER_AUTO_POSITION=1;"
+  mysql -u 'root' -p$MYSQL_PASS -e "START SLAVE;"
+fi
+	
+if [ $(hostname) == node07.local ]; then
+  echo 'server_id = 2' >> /etc/my.cnf
+  systemctl restart mysqld
+  mysql -u 'root' -p$MYSQL_PASS -e "create user 'replicator06'@'%' IDENTIFIED WITH caching_sha2_password BY '$MYSQL_PASS'; GRANT SELECT, SHOW VIEW, PROCESS, REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'replicator06'@'%'; flush privileges;"
+  mysql -u 'root' -p$MYSQL_PASS -e "STOP SLAVE;"
+  mysql -u 'root' -p$MYSQL_PASS -e "CHANGE MASTER TO MASTER_HOST='node06.local', MASTER_USER='replicator07', MASTER_PASSWORD='$MYSQL_PASS', GET_MASTER_PUBLIC_KEY = 1, MASTER_AUTO_POSITION=1;"
+  mysql -u 'root' -p$MYSQL_PASS -e "START SLAVE;"
+  mysql -u 'root' -p$MYSQL_PASS -e "create user 'root'@'%' IDENTIFIED WITH caching_sha2_password BY '$MYSQL_PASS'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'; flush privileges;"
+  
+  #Настраиваем pacemaker
+  pcs cluster auth node06.local node07.local -u hacluster -p 'pacpass'
+  pcs cluster setup --name mysql_cl node06.local node07.local
+  pcs cluster start --all
+  pcs property set stonith-enabled=false
+  pcs property set no-quorum-policy=ignore
+  pcs resource create virtualip ocf:heartbeat:IPaddr2 ip="192.168.10.120" cidr_netmask="24" op monitor interval="10s"
+  pcs resource create mysql_service01 ocf:heartbeat:mysql binary="/usr/sbin/mysqld" config="/etc/my.cnf" datadir="/var/lib/mysql" pid="/var/lib/mysql/mysql.pid" socket="/var/lib/mysql/mysql.sock" op start timeout=20s op stop timeout=20s op monitor interval=20s timeout=30s
+  pcs resource clone mysql_service01
+  pcs constraint colocation add virtualip with mysql_service01 INFINITY
+  pcs constraint order mysql_service01 then virtualip
+  pcs cluster start --all
+fi
+
