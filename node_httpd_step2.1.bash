af8a41a2bfdac5b7e63ae294a4e8530c5b429a5c
seriesninefournine <78902612+seriesninefournine@users.noreply.github.com>
2021-05-28 13:10:17 +0500

    Delete node_httpd_step2.1.bash

--- node_httpd_step2.1.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/glpi-project/glpi/releases/download/9.5.5/glpi-9.5.5.tgz
-tar -xzf glpi-9.5.5.tgz
-cd ./glpi
-mv ./* /var/www/html/
-
-chown -R apache:apache /var/www
